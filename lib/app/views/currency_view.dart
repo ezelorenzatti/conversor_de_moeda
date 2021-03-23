6e24d728a6855c70b827cfc2e883e255058104e6
Ezequiel Lorenzatti <ezelorenzatti@gmail.com>
2021-03-23 13:39:18 -0300

    resolver scroll da tela ao abrir o teclado

--- lib/app/views/currency_view.dart
+++ lib/app/views/currency_view.dart
@@ -13,7 +13,6 @@ class _CurrencyViewState extends State<CurrencyView> {
   final TextEditingController fromText = TextEditingController();
 
   CurrencyController currencyController;
-  bool _keyboardVisible = false;
 
   @override
   void initState() {
@@ -23,59 +22,58 @@ class _CurrencyViewState extends State<CurrencyView> {
 
   @override
   Widget build(BuildContext context) {
-    _keyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 30, right: 30, top: 100, bottom: 20),
-          child: Column(children: [
-            if (!_keyboardVisible)
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 30, right: 30, top: 100, bottom: 20),
+            child: Column(children: [
               Image.asset(
                 MediaFiles.LOGO,
                 width: 120,
                 height: 120,
               ),
-            if (!_keyboardVisible)
               SizedBox(
                 height: 50,
               ),
-            CurrencyBox(
-              selectedItem: currencyController.toCurrency,
-              controller: toText,
-              items: currencyController.currencies,
-              onChanged: (model) {
-                setState(() {
-                  currencyController.toCurrency = model;
-                });
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CurrencyBox(
-              selectedItem: currencyController.fromCurrency,
-              controller: fromText,
-              items: currencyController.currencies,
-              onChanged: (model) {
-                setState(() {
-                  currencyController.fromCurrency = model;
-                });
-              },
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            RaisedButton(
-              color: Colors.amber,
-              child: Text('Converter'),
-              onPressed: () {
-                currencyController.convert();
-              },
-            )
-          ]),
+              CurrencyBox(
+                selectedItem: currencyController.toCurrency,
+                controller: toText,
+                items: currencyController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    currencyController.toCurrency = model;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CurrencyBox(
+                selectedItem: currencyController.fromCurrency,
+                controller: fromText,
+                items: currencyController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    currencyController.fromCurrency = model;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                color: Colors.amber,
+                child: Text('Converter'),
+                onPressed: () {
+                  currencyController.convert();
+                },
+              )
+            ]),
+          ),
         ),
       ),
     );
